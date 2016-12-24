0ddc20c279e6da050208a7a356c93939d451c685
Roman Dvornov <rdvornov@gmail.com>
2016-12-24 16:13:20 +0300

    tweak size output

--- src/ui/src/utils.js
+++ src/ui/src/utils.js
@@ -1,27 +1,27 @@
-function getSize(bytes) {
-    var kBytes = bytes / 1024;
-    var mBytes = bytes / 1024 / 1024;
+function getSize(size) {
+    if (size >= 1024) {
+        size /= 1024;
 
-    if (mBytes >= 1) {
-        return mBytes;
-    } else if (kBytes >= 1) {
-        return kBytes;
+        if (size >= 1024) {
+            size /= 1024;
+        }
     }
 
-    return bytes;
+    return size;
 }
 
-function getPostfix(bytes) {
-    var kBytes = bytes / 1024;
-    var mBytes = bytes / 1024 / 1024;
+function getPostfix(size) {
+    if (size >= 1024) {
+        size /= 1024;
 
-    if (mBytes >= 1) {
-        return 'MB';
-    } else if (kBytes >= 1) {
+        if (size >= 1024) {
+            return 'MB';
+        }
+
         return 'KB';
     }
 
-    return 'B';
+    return 'bytes';
 }
 
 module.exports = {
