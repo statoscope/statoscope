bd6b29c685a66df138c67fdc411c1079f26ee01d
Sergey Melyukov <s.melukov@gmail.com>
2016-12-25 14:24:07 +0300

    fix graph colors minor fix in layout

--- src/ui/src/pages/graph/graph.js
+++ src/ui/src/pages/graph/graph.js
@@ -1,4 +1,5 @@
 var Node = require('basis.ui').Node;
+var DataObject = require('basis.data').DataObject;
 var Value = require('basis.data').Value;
 var addHandler = require('basis.dom.event').addHandler;
 var d3 = require('d3');
@@ -20,13 +21,18 @@ module.exports = Node.subclass({
         }, this);
 
         this.svg = null;
+
+        if (!this.groupsColor) {
+            this.groupsColor = new DataObject();
+        }
+
         var tooltip = new Node({
             container: document.body,
             template: resource('./template/tooltip.tmpl'),
-            bottom: new Value({ value: 0 }),
-            left: new Value({ value: 0 }),
-            visible: new Value({ value: false }),
-            content: new Value({ value: '' }),
+            bottom: new Value({value: 0}),
+            left: new Value({value: 0}),
+            visible: new Value({value: false}),
+            content: new Value({value: ''}),
             binding: {
                 bottom: 'bottom',
                 left: 'left',
@@ -35,11 +41,7 @@ module.exports = Node.subclass({
             }
         });
 
-        Value.query(this, 'data.profile.data.modules').pipe('itemsChanged', function(modules) {
-            if (!modules) {
-                return;
-            }
-
+        Value.query(this, 'data').link(this, function(graph) {
             basis.asap(function() {
                 if (this.svg) {
                     this.svg.remove();
@@ -49,6 +51,8 @@ module.exports = Node.subclass({
                 this.updateGraphSize();
                 var width = this.svg.node().clientWidth;
                 var height = parseInt(this.svg.attr('height'));
+                var groupsColor = this.groupsColor.data;
+                var dragging = false;
 
                 var simulation = d3.forceSimulation()
                     .force('link', d3.forceLink().id(function(d) {
@@ -57,29 +61,7 @@ module.exports = Node.subclass({
                     .force('charge', d3.forceManyBody())
                     .force('center', d3.forceCenter(width / 2, height / 2));
 
-                var nodes = [];
-                var links = [];
-                var groupColor = {
-                    all: '#1f77b4',
-                    css: '#563d7c',
-                    js: '#f1e05a',
-                    html: '#e44b23'
-                };
-
-                modules.forEach(function(module) {
-                    var group = module.data.name.split('.').pop() || 'all';
-
-                    nodes.push({ id: module.data.id, name: module.data.name, group: group });
-                    module.data.reasons.forEach(function(reason) {
-                        links.push({ source: reason.data.id, target: module.data.id });
-                    });
-                });
-
-                var graph = {
-                    nodes: nodes,
-                    links: links
-                };
-
+                var color = d3.scaleOrdinal(d3.schemeCategory10);
                 var link = this.svg.append('g')
                     .attr('class', 'links')
                     .selectAll('line')
@@ -94,17 +76,26 @@ module.exports = Node.subclass({
                     .data(graph.nodes)
                     .enter().append('circle')
                     .attr('r', 5)
-                    .attr('stroke-width', '1.5px')
+                    .attr('stroke-width', function(d) {
+                        return d.main ? '2.5px' : '1.5px';
+                    })
+                    .attr('stroke', function(d) {
+                        return d.main ? '#d62728' : '#fff';
+                    })
                     .attr('fill', function(d) {
-                        return groupColor[d.group] || groupColor.all;
+                        return color(groupsColor[d.group] || groupsColor.all);
                     })
                     .on('mouseover', function(sender) {
-                        tooltip.content.set(sender.name);
-                        tooltip.visible.set(true);
+                        if (!dragging) {
+                            tooltip.content.set(sender.name);
+                            tooltip.visible.set(true);
+                        }
                     })
                     .on('mousemove', function() {
-                        tooltip.bottom.set(window.innerHeight - event.pageY + 10);
-                        tooltip.left.set(event.pageX - 10);
+                        if (!dragging) {
+                            tooltip.bottom.set(window.innerHeight - event.pageY + 10);
+                            tooltip.left.set(event.pageX - 10);
+                        }
                     })
                     .on('mouseout', function() {
                         tooltip.visible.set(false);
@@ -151,6 +142,7 @@ module.exports = Node.subclass({
                     }
                     d.fx = d.x;
                     d.fy = d.y;
+                    dragging = true;
                 }
 
                 function dragged(d) {
@@ -164,6 +156,7 @@ module.exports = Node.subclass({
                     }
                     d.fx = null;
                     d.fy = null;
+                    dragging = false;
                 }
             }, this);
         }.bind(this));
