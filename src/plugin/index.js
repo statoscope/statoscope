49f76295e5812165fd94ae3ac46b11e39b8982f4
Sergey Melyukov <s.melukov@gmail.com>
2017-03-24 02:17:16 +0300

    (plugin): generate modules links (plugin): generate retained modules (plugin): generate exclusive modules

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,12 +2,12 @@
 
 var parseQuery = require('loader-utils/lib/parseQuery.js');
 var webpack = require('webpack');
+var webpackVersion = require.main.require('webpack/package.json').version;
 var RequestShortener = require('webpack/lib/RequestShortener');
 var rempl = require('rempl');
 var path = require('path');
 var fs = require('fs');
 
-var COMPILATION;
 var handledFiles;
 var requestShortener;
 
@@ -59,6 +59,29 @@ function deepExtend(target) {
     return target;
 }
 
+function getRetained(module, modulesMap, exclude) {
+    var stack = [module.id];
+    var visited = {};
+
+    while (stack.length) {
+        var currentModuleId = stack.pop();
+        var currentModule = modulesMap[currentModuleId];
+
+        if (!exclude || currentModule != exclude) {
+            for (var i = 0; i < currentModule.dependencies.length; i++) {
+                var dependency = currentModule.dependencies[i];
+
+                if (!visited[dependency]) {
+                    stack.push(dependency);
+                    visited[dependency] = true;
+                }
+            }
+        }
+    }
+
+    return Object.keys(visited);
+}
+
 function getModuleId(module) {
     return module.index + '_' + module.readableIdentifier(requestShortener);
 }
@@ -108,178 +131,234 @@ function getModuleLoaders(module) {
 
 function RuntimeAnalyzerPlugin(options) {
     var defaultOptions = {
-        onlyWatchMode: true,
+        watchModeOnly: true,
         ui: path.resolve(__dirname, '../../dist/script.js')
     };
 
     this.options = deepExtend({}, defaultOptions, options);
 }
 
-RuntimeAnalyzerPlugin.prototype.apply = function(compiler) {
-    var options = this.options;
-    var pluginMode = options.onlyWatchMode ? 'watch-run' : 'run';
+function createPublisher(compiler, options) {
+    var getWebUI = rempl.scriptFromFile(options.ui);
+    var publisher = rempl.createPublisher('webpack-analyzer', function(settings, callback) {
+        if (settings.dev) {
+            return callback(null, 'url', 'http://localhost:8001/src/ui/');
+        }
 
-    compiler.plugin(pluginMode, function(watching, done) {
-        this.transport = rempl.createPublisher('webpack-analyzer', function(settings, callback) {
-            if (settings.dev) {
-                return callback(null, 'url', 'http://localhost:8001/src/ui/');
+        getWebUI(settings, callback);
+    });
+
+    var statusChannel = publisher.ns('status');
+    var progressChannel = publisher.ns('progress');
+    var profileChannel = publisher.ns('profile');
+    var COMPILATION;
+
+    compiler.plugin('compilation', function(compilation) {
+        COMPILATION = compilation;
+        requestShortener = new RequestShortener(compilation.options.context || process.cwd());
+    });
+
+    compiler.apply(new webpack.ProgressPlugin(function(percent) {
+        statusChannel.publish('compiling');
+        progressChannel.publish(percent);
+    }));
+
+    compiler.plugin('emit', function(compilation, done) {
+        var assets = [];
+        var modulesMap = {};
+        var entryPoints = {};
+        var modules;
+        var profile;
+
+        handledFiles = {};
+
+        for (var assetName in compilation.assets) {
+            if (compilation.assets.hasOwnProperty(assetName)) {
+                assets.push({
+                    name: assetName,
+                    size: compilation.assets[assetName].size()
+                });
             }
+        }
 
-            rempl.scriptFromFile(options.ui)(settings, callback);
-        });
+        modules = compilation.modules.map(function(module) {
+            var modulesTypeMap = {
+                NormalModule: 'normal',
+                MultiModule: 'multi',
+                ContextModule: 'context',
+                DelegatedModule: 'delegated',
+                ExternalModule: 'external'
+            };
+            var moduleType = modulesTypeMap[module.constructor.name] || 'unknown';
+            var resource = module.resource ? handleFile(module.resource) : null;
 
-        compiler.plugin('compilation', function(compilation) {
-            COMPILATION = compilation;
-            requestShortener = new RequestShortener(compilation.options.context || process.cwd());
-        });
+            // webpack 1.x capability
+            if (webpackVersion[0] == 1) {
+                var identifier = module.identifier();
 
-        compiler.apply(new webpack.ProgressPlugin(function(percent) {
-            this.transport.ns('status').publish('compiling');
-            this.transport.ns('progress').publish(percent);
-        }.bind(this)));
-
-        compiler.plugin('emit', function(compilation, done) {
-            var assets = [];
-            var modules;
-            var profile;
-            var webpackVersion = require.main.require('webpack/package.json').version;
-
-            handledFiles = {};
-
-            for (var assetName in compilation.assets) {
-                if (compilation.assets.hasOwnProperty(assetName)) {
-                    assets.push({
-                        name: assetName,
-                        size: compilation.assets[assetName].size()
-                    });
+                if (!identifier.indexOf('multi')) {
+                    moduleType = 'multi';
+                } else if (module.regExp) {
+                    moduleType = 'context';
+                } else if (!identifier.indexOf('delegated')) {
+                    moduleType = 'delegated';
+                } else if (!identifier.indexOf('external')) {
+                    moduleType = 'external';
+                } else if (resource) {
+                    moduleType = 'normal';
+                } else {
+                    moduleType = 'unknown';
                 }
             }
 
-            modules = compilation.modules.map(function(module) {
-                var modulesTypeMap = {
-                    NormalModule: 'normal',
-                    MultiModule: 'multi',
-                    ContextModule: 'context',
-                    DelegatedModule: 'delegated',
-                    ExternalModule: 'external'
-                };
-                var moduleType = modulesTypeMap[module.constructor.name] || 'unknown';
-                var resource = module.resource ? handleFile(module.resource) : null;
-
-                // webpack 1.x capability
-                if (webpackVersion[0] == 1) {
-                    var identifier = module.identifier();
-
-                    if (!identifier.indexOf('multi')) {
-                        moduleType = 'multi';
-                    } else if (module.regExp) {
-                        moduleType = 'context';
-                    } else if (!identifier.indexOf('delegated')) {
-                        moduleType = 'delegated';
-                    } else if (!identifier.indexOf('external')) {
-                        moduleType = 'external';
-                    } else if (resource) {
-                        moduleType = 'normal';
-                    } else {
-                        moduleType = 'unknown';
-                    }
-                }
-
-                var moduleInfo = {
-                    id: getModuleId(module),
-                    index: module.index,
-                    type: moduleType,
-                    name: module.readableIdentifier(requestShortener),
-                    size: module.size(),
-                    rawRequest: module.rawRequest,
-                    userRequest: module.userRequest,
-                    context: module.context,
-                    resource: resource,
-                    reasons: module.reasons.filter(function(reason) {
-                        return reason.dependency && reason.module;
-                    }).map(function(reason) {
-                        var reasonInfo = {
-                            module: getModuleId(reason.module),
-                            // todo handle string value
-                            loc: typeof reason.dependency.loc == 'string' ? null : reason.dependency.loc
-                        };
-
-                        if (reasonInfo.loc) {
-                            if (reasonInfo.loc.start && isFinite(reasonInfo.loc.start.column)) {
-                                reasonInfo.loc.start.column++
-                            }
-
-                            if (reasonInfo.loc.end && isFinite(reasonInfo.loc.end.column)) {
-                                reasonInfo.loc.end.column++
-                            }
-                        }
-
-                        return reasonInfo;
-                    }),
-                    loaders: getModuleLoaders(module)
-                };
-
-                return moduleInfo;
-            });
-
-            profile = {
-                version: webpackVersion,
-                hash: compilation.hash,
-                context: compilation.compiler.context,
-                assets: assets,
-                chunks: compilation.chunks.map(function(chunk) {
-                    return {
-                        id: chunk.id,
-                        name: chunk.name,
-                        size: chunk.size({}),
-                        hash: chunk.renderedHash,
-                        files: chunk.files,
-                        modules: chunk.modules.map(function(module) {
-                            return getModuleId(module);
-                        }),
-                        rendered: chunk.rendered,
-                        // webpack 1.x capability
-                        initial: typeof chunk.isInitial == 'function' ? chunk.isInitial() : chunk.initial,
-                        // webpack 1.x capability
-                        entry: typeof chunk.hasRuntime == 'function' ? chunk.hasRuntime() : chunk.entry
-                    };
+            var moduleInfo = {
+                id: getModuleId(module),
+                index: module.index,
+                type: moduleType,
+                name: module.readableIdentifier(requestShortener),
+                size: module.size(),
+                rawRequest: module.rawRequest,
+                userRequest: module.userRequest,
+                context: module.context,
+                resource: resource,
+                dependencies: module.dependencies.filter(function(dependency) {
+                    return dependency.module;
+                }).map(function(dependency) {
+                    return getModuleId(dependency.module);
                 }),
-                modules: modules,
-                errors: compilation.errors.map(function(error) {
-                    return {
-                        message: error.message,
-                        module: error.module && getModuleId(error.module)
-                    };
+                reasons: module.reasons.filter(function(reason) {
+                    return reason.dependency && reason.module;
+                }).map(function(reason) {
+                    return getModuleId(reason.module);
                 }),
-                warnings: compilation.warnings.map(function(warning) {
-                    return {
-                        message: warning.message,
-                        module: warning.module && getModuleId(warning.module)
-                    };
-                })
+                loaders: getModuleLoaders(module)
             };
 
-            this.transport.ns('profile').publish(profile);
-            done();
-        }.bind(this));
+            modulesMap[moduleInfo.id] = moduleInfo;
+            moduleInfo.isEntry = !moduleInfo.reasons.length;
 
-        compiler.plugin('compile', function() {
-            this.transport.ns('status').publish('compiling');
-        }.bind(this));
+            if (moduleInfo.isEntry) {
+                entryPoints[moduleInfo.id] = true;
+            }
 
-        compiler.plugin('invalid', function() {
-            this.transport.ns('status').publish('invalidated');
-        }.bind(this));
+            return moduleInfo;
+        });
 
-        compiler.plugin('done', function() {
-            this.transport.ns('status').publish(COMPILATION.errors.length ? 'failed' : 'success');
-        }.bind(this));
+        entryPoints = Object.keys(entryPoints);
 
-        compiler.plugin('failed', function() {
-            this.transport.ns('status').publish('failed');
-        }.bind(this));
+        // console.time('Retained + Exclusive');
+
+        modules.forEach(function(module) {
+            var allSubtreeExceptSelected = {};
+
+            module.retained = getRetained(module, modulesMap);
+
+            if (module.retained.indexOf(module.id) == -1) {
+                module.retained.push(module.id);
+            }
+
+            for (var i = 0; i < entryPoints.length; i++) {
+                var nodes = getRetained(modulesMap[entryPoints[i]], modulesMap, module);
+
+                for (var j = 0; j < nodes.length; j++) {
+                    allSubtreeExceptSelected[nodes[j]] = true;
+                }
+            }
+
+            module.exclusive = module.retained.filter(function(exclusive) {
+                return exclusive != module.id && !allSubtreeExceptSelected[exclusive];
+            });
+
+            var ix = module.retained.indexOf(module.id);
+
+            if (ix > -1) {
+                module.retained.splice(ix, 1)
+            }
+        });
+
+        // console.timeEnd('Retained + Exclusive');
+
+        profile = {
+            version: webpackVersion,
+            hash: compilation.hash,
+            context: compilation.compiler.context,
+            assets: assets,
+            chunks: compilation.chunks.map(function(chunk) {
+                return {
+                    id: chunk.id,
+                    name: chunk.name,
+                    size: chunk.size({}),
+                    hash: chunk.renderedHash,
+                    files: chunk.files,
+                    modules: chunk.modules.map(function(module) {
+                        return getModuleId(module);
+                    }),
+                    rendered: chunk.rendered,
+                    // webpack 1.x capability
+                    initial: typeof chunk.isInitial == 'function' ? chunk.isInitial() : chunk.initial,
+                    // webpack 1.x capability
+                    entry: typeof chunk.hasRuntime == 'function' ? chunk.hasRuntime() : chunk.entry
+                };
+            }),
+            modules: modules,
+            links: modules.reduce(function(prev, current) {
+                var links = current.dependencies.map(function(dependency) {
+                    return {
+                        from: current.id,
+                        to: dependency
+                    };
+                });
+
+                return prev.concat(links);
+            }, []),
+            errors: compilation.errors.map(function(error) {
+                return {
+                    message: error.message,
+                    module: error.module && getModuleId(error.module)
+                };
+            }),
+            warnings: compilation.warnings.map(function(warning) {
+                return {
+                    message: warning.message,
+                    module: warning.module && getModuleId(warning.module)
+                };
+            })
+        };
+
+        profileChannel.publish(profile);
         done();
-    }.bind(this));
+    });
+
+    compiler.plugin('compile', function() {
+        statusChannel.publish('compiling');
+    });
+
+    compiler.plugin('invalid', function() {
+        statusChannel.publish('invalidated');
+    });
+
+    compiler.plugin('done', function() {
+        statusChannel.publish(COMPILATION.errors.length ? 'failed' : 'success');
+    });
+
+    compiler.plugin('failed', function() {
+        statusChannel.publish('failed');
+    });
+}
+
+RuntimeAnalyzerPlugin.prototype.apply = function(compiler) {
+    var options = this.options;
+    var pluginMode = options.watchModeOnly ? 'watch-run' : 'run';
+    var inited = false;
+
+    compiler.plugin(pluginMode, function(watching, done) {
+        if (!inited) {
+            createPublisher(compiler, options);
+            inited = true;
+        }
+
+        done();
+    });
 };
 
 module.exports = RuntimeAnalyzerPlugin;
