1d337d0fec9d1a779e47858db31109bf2134b8c2
Sergey Melyukov <s.melukov@gmail.com>
2017-03-29 13:11:25 +0300

    (ui): reworking status bar

--- src/ui/src/ui/status/index.js
+++ src/ui/src/ui/status/index.js
@@ -1,7 +1,6 @@
 var Node = require('basis.ui').Node;
 var Value = require('basis.data').Value;
 var type = require('app.type');
-var env = require('app.type').Env;
 
 module.exports = Node.subclass({
     delegate: type.Profile(),
@@ -10,18 +9,13 @@ module.exports = Node.subclass({
         version: Value.query('data.version').as(function(version) {
             return version || 'n/a';
         }),
-        hash: Value.query('data.hash').as(function(hash) {
-            return hash || 'n/a';
-        }),
         status: 'data:',
         assets: Value.query(type.Asset.all, 'itemCount'),
         modules: Value.query(type.Module.allWrapper, 'itemCount'),
         chunks: Value.query(type.Chunk.all, 'itemCount'),
-        errors: Value.query(type.Error.all, 'itemCount'),
-        warnings: Value.query(type.Warning.all, 'itemCount'),
         modulesTable: 'satellite:',
         assetsTable: 'satellite:',
-        envName: Value.query(env, 'data.name'),
-        envVersion: Value.query(env, 'data.version')
+        envName: Value.query(type.Env, 'data.name'),
+        envVersion: Value.query(type.Env, 'data.version')
     }
 });
