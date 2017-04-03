0c8e8ed1f6b962371b446cd2966660e7e872a3b3
Sergey Melyukov <smelyukov@avito.ru>
2017-04-03 18:48:53 +0300

    (ui): move openInEditor in transport

--- src/ui/src/transport.js
+++ src/ui/src/transport.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+    openInEditor: function(path) {
+        if (!this.api) {
+            return;
+        }
+
+        this.api.callRemote('openInEditor', path, function(result) {
+            if (!result.ok) {
+                if (result.error) {
+                    alert(result.error);
+                } else {
+                    alert('Unknown error while opening in editor');
+                }
+            }
+        });
+    }
+};
