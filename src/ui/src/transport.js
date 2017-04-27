76229cde1459e1cbdf756d57ebe8241f5a8a7002
Roman Dvornov <rdvornov@gmail.com>
2017-04-27 18:45:20 +0300

    remove redundant script in UI build

--- src/ui/src/transport.js
+++ src/ui/src/transport.js
@@ -1,3 +1,7 @@
+// in PROD mode UI is distributed as `script` and sandbox add rempl to global
+// in DEV mode UI is distributed as `url` and we need include a rempl script
+/** @cut */ var rempl = require('../../../node_modules/rempl/dist/rempl.js');
+
 module.exports = {
     api: rempl.getSubscriber(),
     editorEnv: rempl.getEnv('editor'),
