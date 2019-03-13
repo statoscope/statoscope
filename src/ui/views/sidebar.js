f0014f1d832f08d0bd992be571e7bb908771df22
Sergey Melyukov <s.melukov@gmail.com>
2019-03-14 01:06:30 +0300

    added node_modules tab to sidebar

--- src/ui/views/sidebar.js
+++ src/ui/views/sidebar.js
@@ -1,19 +1,56 @@
 export default function (discovery) {
   discovery.view.define('sidebar', {
-    view: 'content-filter',
-    content: {
-      view: 'list',
-      data: `
-        data.input.files.[no #.filter or path~=#.filter]
-        .({
-            caption: path,
-            href: "#file:" + path.encodeURIComponent(),
-            match: #.filter
-        })
-      `,
-      emptyText: 'No matches',
-      item: 'toc-item'
-    }
+    view: 'tabs',
+    name:
+      'messagesTabs',
+    tabs:
+      [
+        { value: 'files', text: 'Files' },
+        { value: 'node_modules', text: 'Node Modules' }
+      ],
+    content:
+      {
+        view: 'content-filter',
+        content:
+          {
+            view: 'switch',
+            content:
+              [
+                {
+                  when: '#.messagesTabs="files"',
+                  content: {
+                    view: 'list',
+                    data: `
+                      data.input.files.sort(<path>).[no #.filter or path~=#.filter]
+                      .({
+                          caption: path,
+                          href: "#file:" + path.encodeURIComponent(),
+                          match: #.filter
+                      })
+                    `,
+                    emptyText: 'No matches',
+                    item: 'toc-item'
+                  }
+                },
+                {
+                  when: '#.messagesTabs="node_modules"',
+                  content: {
+                    view: 'list',
+                    data: `
+                      data.input.files.nodeModule.name.sort().[no #.filter or $~=#.filter]
+                      .({
+                          caption: $,
+                          href: "#node-module:" + $.encodeURIComponent(),
+                          match: #.filter
+                      })
+                    `,
+                    emptyText: 'No matches',
+                    item: 'toc-item'
+                  }
+                }
+              ]
+          }
+      }
   }, {
     tag: false
   });
