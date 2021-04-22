6966139d2ad22f3eba6269f439ae11c523750474
Sergey Melyukov <s.melukov@gmail.com>
2021-04-22 23:31:56 +0300

    feat: improve starting performance

--- packages/ui-webpack/src/init/prepare/entity.js
+++ packages/ui-webpack/src/init/prepare/entity.js
@@ -1,5 +1,24 @@
+function warnCache(entities, getId, cache) {
+  if (Array.isArray(entities) || entities instanceof Set) {
+    for (const entity of entities) {
+      cache.set(getId(entity), entity);
+    }
+  } else if (entities instanceof Map) {
+    for (const [, entity] of entities) {
+      cache.set(getId(entity), entity);
+    }
+  } else {
+    for (const name in entities) {
+      const entity = entities[name];
+      cache.set(getId(entity), entity);
+    }
+  }
+}
+
 export function makeEntityResolver(entities, getId) {
   const cache = new Map();
+
+  warnCache(entities, getId, cache)
 
   return (id) => {
     const cached = cache.get(id);
