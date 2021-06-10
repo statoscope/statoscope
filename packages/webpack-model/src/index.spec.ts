28d3e7d0ccb306d33e2d3c8b408c20a8ddd9dc13
Sergey Melyukov <s.melukov@gmail.com>
2021-06-10 19:32:03 +0300

    test(webpack-model): add tests for index and jora-helpers

---
+++ packages/webpack-model/src/index.spec.ts
@@ -0,0 +1,12 @@
+import stats from '../../../test/bundles/simple/stats-prod.json';
+import { prepareWithJora } from './';
+
+test('prepareWithJora', () => {
+  const j = prepareWithJora({ name: 'stats.js', data: stats });
+  expect(j).toMatchObject({
+    files: [{ name: 'stats.js' }],
+    compilations: [{ hash: stats.hash }],
+  });
+  expect(j.query('name')).toEqual(['stats.js']);
+  expect(j.query('foo', { foo: 'bar' })).toEqual('bar');
+});
--- packages/webpack-model/src/jora-helpers.spec.ts
+++ packages/webpack-model/src/jora-helpers.spec.ts
@@ -1,5 +1,5 @@
 import stats from '../../../test/bundles/simple/stats-prod.json';
-import normalize from './normalize';
+import normalize, { NormalizedAsset, NormalizedChunk } from './normalize';
 import makeHelpers, { ResolvedStats } from './jora-helpers';
 
 const normalized = normalize({ name: 'stats.js', data: stats });
@@ -14,12 +14,97 @@ test('resolveStat', () => {
   expect(resolvedStats?.compilation).toBe(firstCompilation);
 });
 
-test('statName', () => {
-  const resolvedStats = helpers.resolveStat(hash);
-  expect(helpers.statName(resolvedStats as ResolvedStats)).toMatch(/stats\.js \(.+?\)/);
-});
-
 test('resolveChunk', () => {
   const chunk = firstCompilation.chunks[0];
   expect(helpers.resolveChunk(chunk.id, hash)).toBe(chunk);
+});
+
+test('resolveAsset', () => {
+  const asset = firstCompilation.assets[0];
+  expect(helpers.resolveAsset(asset.name, hash)).toBe(asset);
+});
+
+test('resolveModule', () => {
+  const module = firstCompilation.modules[0];
+  expect(helpers.resolveModule(module.name, hash)).toBe(module);
+});
+
+test('resolvePackage', () => {
+  const thePackage = firstCompilation.nodeModules[0];
+  expect(helpers.resolvePackage(thePackage.name, hash)).toBe(thePackage);
+});
+
+test('resolveCompilation', () => {
+  expect(helpers.resolveCompilation(firstCompilation.hash)).toBe(firstCompilation);
+});
+
+test('statName', () => {
+  const resolvedStats = helpers.resolveStat(hash);
+  expect(helpers.statName(resolvedStats as ResolvedStats)).toMatch(/stats\.js \(.+?\)/);
+  expect(helpers.statName()).toBe('unknown');
+});
+
+test('moduleSize', () => {
+  const module = firstCompilation.modules[0];
+  const size = helpers.moduleSize(module);
+  expect(typeof size === 'number').toBe(true);
+  expect(size).toBeGreaterThan(0);
+});
+
+test.each([
+  {
+    chunk: {
+      id: 123,
+      name: 'foo',
+      names: [],
+      reason: 'quux',
+    },
+    name: 'foo [quux]',
+  },
+  {
+    chunk: {
+      id: 123,
+      name: 'foo',
+      names: ['bar', 'baz'],
+      reason: 'quux',
+    },
+    name: 'bar, baz [quux]',
+  },
+
+  {
+    chunk: {
+      id: 123,
+      name: 'foo',
+      names: ['foo'],
+    },
+    name: 'foo',
+  },
+  {
+    chunk: {
+      id: 123,
+      names: ['foo'],
+    },
+    name: 'foo',
+  },
+] as Array<{ chunk: Partial<NormalizedChunk>; name: string }>)(
+  'chunkName: $name',
+  ({ chunk, name }) => {
+    expect(helpers.chunkName(chunk as NormalizedChunk)).toBe(name);
+  }
+);
+
+test('getTotalFilesSize', () => {
+  expect(
+    helpers.getTotalFilesSize(
+      Object.assign(
+        {} as NormalizedAsset,
+        {
+          files: [
+            { name: 'one', size: 100 },
+            { name: 'two', size: 50 },
+          ],
+        } as Partial<NormalizedAsset>
+      )
+    )
+  ).toBe(150);
 });
