b54131e2b45227026e6cb1edb20221ddfd516295
Sergey Melyukov <s.melukov@gmail.com>
2021-06-13 03:19:22 +0300

    revert(webpack-ui): webpack config

--- packages/webpack-ui/webpack.config.js
+++ packages/webpack-ui/webpack.config.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
-const Statoscope = require('../webpack-plugin').default;
 
 const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
@@ -37,11 +36,6 @@ function makeConfig(config) {
         ],
       },
       plugins: [
-        new Statoscope({
-          saveTo: `analyze/statoscope-[name]-[hash].html`,
-          saveStatsTo: `analyze/stats-[name]-[hash].json`,
-          open: 'file',
-        }),
         new webpack.EnvironmentPlugin({
           STATOSCOPE_VERSION: require('./package.json').version,
         }),
