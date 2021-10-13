85e47ae6e31bf85f860e0446a735d98ea48b48e1
Sergey Melyukov <s.melukov@gmail.com>
2021-10-13 19:31:18 +0300

    test(cli): refactor tests

--- packages/cli/src/commands/injectReport.spec.ts
+++ packages/cli/src/commands/injectReport.spec.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { spawn } from 'child_process';
+import { mergeReports } from './injectReport';
 
 const rootPath = path.resolve(__dirname, '../../../../');
 const outputDir = path.join(
@@ -22,20 +22,14 @@ const stats = [
 
 fs.mkdirSync(outputDir, { recursive: true });
 
-jest.mock('fs', () => {
-  const ofs = jest.requireActual('fs');
-  return {
-    ...ofs,
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    readFileSync(name: string, ...args: unknown[]): unknown {
-      // eslint-disable-next-line eqeqeq
-      if (name == '0') {
-        return '';
-      }
-      return ofs.readFileSync(name, ...args);
-    },
-  };
-});
+jest.mock('../../package.json', () => ({
+  ...jest.requireActual('../../package.json'),
+  version: '8.8.8',
+}));
+jest.mock('@statoscope/stats-extension-custom-reports/package.json', () => ({
+  ...jest.requireActual('@statoscope/stats-extension-custom-reports/package.json'),
+  version: '8.8.8',
+}));
 
 const reports = [
   require.resolve(
@@ -48,29 +42,9 @@ const reports = [
 
 test.each(stats)('%s,', async (filename) => {
   for (const report of reports) {
-    const proc = spawn(path.resolve(__dirname, '../../bin/cli.js'), [
-      'inject-report',
-      '--input',
-      path.join(statsDir, filename),
-      '--report',
-      report,
-    ]);
+    const parsed = JSON.parse(fs.readFileSync(path.join(statsDir, filename), 'utf-8'));
+    const parsedReport = JSON.parse(fs.readFileSync(report, 'utf-8'));
 
-    const output = await new Promise((resolve, reject) => {
-      const stdoutLines: string[] = [];
-      const stderrLines: string[] = [];
-      proc.stdout?.on('data', (data) => stdoutLines.push(data.toString()));
-      proc.stderr?.on('data', (data) => stderrLines.push(data.toString()));
-      proc.on('exit', (code) => {
-        if (!code) {
-          resolve(stdoutLines);
-        } else {
-          console.error(stderrLines);
-          reject(new Error(`Exit code ${code}`));
-        }
-      });
-    });
-
-    expect(output).toMatchSnapshot();
+    expect(await mergeReports([parsedReport].flat(), parsed)).toMatchSnapshot();
   }
 });
--- packages/cli/src/commands/injectReport.ts
+++ packages/cli/src/commands/injectReport.ts
@@ -8,6 +8,56 @@ import { waitFinished } from '@statoscope/report-writer/dist/utils';
 import { Webpack } from '@statoscope/webpack-model/webpack';
 import Compilation = Webpack.Compilation;
 
+export function mergeReports(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  reports: any[],
+  parsed: Compilation
+): Compilation {
+  parsed.__statoscope ??= {};
+  parsed.__statoscope.extensions ??= [];
+  const customReportsExtensionIx = parsed.__statoscope.extensions.findIndex(
+    (ext) => ext.descriptor.name === '@statoscope/stats-extension-custom-reports'
+  );
+  const customReportsExtension =
+    customReportsExtensionIx > -1
+      ? parsed.__statoscope.extensions[customReportsExtensionIx]
+      : null;
+  const customReportGenerator = new Generator();
+  if (customReportsExtension?.payload) {
+    const payload = customReportsExtension.payload as Payload;
+
+    for (const compilationItem of payload.compilations) {
+      for (const report of compilationItem.reports) {
+        customReportGenerator.handleReport(report);
+      }
+    }
+  }
+
+  for (const report of reports) {
+    if (
+      report &&
+      typeof report === 'object' &&
+      report.constructor === Object &&
+      typeof report.id !== 'undefined' &&
+      typeof report.view !== 'undefined'
+    ) {
+      customReportGenerator.handleReport(report);
+    } else {
+      throw new Error(
+        `Can't add a report. A valid report should contain id and view fields`
+      );
+    }
+  }
+
+  if (customReportsExtension) {
+    parsed.__statoscope.extensions.splice(customReportsExtensionIx, 1);
+  }
+
+  parsed.__statoscope.extensions.push(customReportGenerator.get());
+
+  return parsed;
+}
+
 export default function (yargs: Argv): Argv {
   return yargs.command(
     'inject-report',
@@ -46,50 +96,8 @@ export default function (yargs: Argv): Argv {
       }
 
       const parsed = (await parseChunked(fs.createReadStream(argv.input))) as Compilation;
-
-      parsed.__statoscope ??= {};
-      parsed.__statoscope.extensions ??= [];
-      const customReportsExtensionIx = parsed.__statoscope.extensions.findIndex(
-        (ext) => ext.descriptor.name === '@statoscope/stats-extension-custom-reports'
-      );
-      const customReportsExtension =
-        customReportsExtensionIx > -1
-          ? parsed.__statoscope.extensions[customReportsExtensionIx]
-          : null;
-      const customReportGenerator = new Generator();
-      if (customReportsExtension?.payload) {
-        const payload = customReportsExtension.payload as Payload;
-
-        for (const compilationItem of payload.compilations) {
-          for (const report of compilationItem.reports) {
-            customReportGenerator.handleReport(report);
-          }
-        }
-      }
-
-      for (const report of reports) {
-        if (
-          report &&
-          typeof report === 'object' &&
-          report.constructor === Object &&
-          typeof report.id !== 'undefined' &&
-          typeof report.view !== 'undefined'
-        ) {
-          customReportGenerator.handleReport(report);
-        } else {
-          throw new Error(
-            `Can't add a report. A valid report should contain id and view fields`
-          );
-        }
-      }
-
-      if (customReportsExtension) {
-        parsed.__statoscope.extensions.splice(customReportsExtensionIx, 1);
-      }
-
-      parsed.__statoscope.extensions.push(customReportGenerator.get());
-
-      const outputStream = stringifyStream(parsed);
+      const merged = mergeReports(reports, parsed);
+      const outputStream = stringifyStream(merged);
 
       outputStream.pipe(process.stdout);
       await waitFinished(outputStream);
