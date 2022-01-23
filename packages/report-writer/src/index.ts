661dd01ebee7c4f0831128c8f4dfe5e485fed031
Sergey Melyukov <s.melukov@gmail.com>
2022-01-23 19:49:23 +0300

    chore(webpack-ui): use new logo
    chore(report-writer): use logo as loading spinner
    # Please, use semantic commit message:
    # type(scope): message
    # or
    # type: message
    #
    # Types are:
    # - build - code build changes (webpack config, etc)
    # - ci - CI changes (GitHub actions, CI scripts)
    # - docs - documentation changes
    # - feat - new features
    # - fix - bug fixes
    # - perf - performance improvements
    # - refactor - code refactoring (also types changes)
    # - style - code style changes (also eslint/prettier config changes)
    # - test - add/fix/remove tests, or change jest configuration
    # - chore - everything else
    #
    # The scope is a package name that changes related to
    #
    # For example:
    # test(cli): add foo command
    # or
    # docs: fix root readme typos
    # Please, use semantic commit message:
    # type(scope): message
    # or
    # type: message
    #
    # Types are:
    # - build - code build changes (webpack config, etc)
    # - ci - CI changes (GitHub actions, CI scripts)
    # - docs - documentation changes
    # - feat - new features
    # - fix - bug fixes
    # - perf - performance improvements
    # - refactor - code refactoring (also types changes)
    # - style - code style changes (also eslint/prettier config changes)
    # - test - add/fix/remove tests, or change jest configuration
    # - chore - everything else
    #
    # The scope is a package name that changes related to
    #
    # For example:
    # test(cli): add foo command
    # or
    # docs: fix root readme typos

--- packages/report-writer/src/index.ts
+++ packages/report-writer/src/index.ts
@@ -3,14 +3,17 @@ import { PassThrough, Readable, Writable } from 'stream';
 import makeChunkToScriptWriter from './chunkToScriptWriter';
 
 export type InitArg = { id: string; data: unknown }[];
-export type ScriptItem =
+export type AssetItem =
+  | string
   | {
       type: 'path';
       path: string;
     }
   | { type: 'raw'; content: string };
+export type ScriptItem = AssetItem;
 export type Options = {
-  scripts: Array<ScriptItem | string>;
+  scripts?: ScriptItem[];
+  html?: string[];
   init: string | ((data: InitArg) => void);
   jsonExtAPIName?: string;
 };
@@ -54,29 +57,96 @@ export default class HTMLWriter {
   }
 }
 
+function handleAsset(type: 'script' | 'style', asset: AssetItem): string {
+  if (typeof asset === 'string') {
+    asset = { type: 'path', path: asset };
+  }
+
+  const content =
+    asset.type === 'path'
+      ? fs.readFileSync(require.resolve(asset.path), 'utf8')
+      : asset.content;
+
+  return `<${type}>${content}</${type}>`;
+}
+
 function writeHeader(stream: Writable, options: Options): void {
   stream.write(`<!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
+    <style>
+      html, body {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        border: none;
+        -webkit-text-size-adjust: 100%;
+      }
+      
+      #loading {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+            -ms-flex-pack: center;
+                justify-content: center;
+      }
+      
+      .logo {
+        position: relative;
+        top: 20%;
+        width: 10vw;
+        height: 10vw;
+      }
+      
+      .logo > svg {
+        -webkit-animation: rotate 3s ease infinite;
+                animation: rotate 3s ease infinite;
+        width: 100%;
+        height: 100%;
+      }
+      
+      @-webkit-keyframes rotate {
+        0% {
+          -webkit-transform: rotate(0deg);
+                  transform: rotate(0deg);
+        }
+        25% {
+        }
+        50% {
+          -webkit-transform: rotate(1440deg);
+                  transform: rotate(1440deg);
+        }
+        75% {
+        }
+        100% {
+          -webkit-transform: rotate(0deg);
+                  transform: rotate(0deg);
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div id="loading">
+      <div class="logo">
+        <svg width="144" height="144" viewBox="0 0 144 144" fill="none" xmlns="http://www.w3.org/2000/svg"><mask id="a" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="144" height="144"><circle cx="72" cy="72" r="72" fill="#fff"/></mask><g mask="url(#a)"><path d="M144 0H0v144h144V0Z" fill="#10255F"/><rect x="-67.999" y="-92" width="232" height="48" rx="24" transform="rotate(30 -68 -92)" fill="#4581B7"/><rect x="-22.718" y="-10.43" width="232" height="48" rx="24" transform="rotate(30 -22.718 -10.43)" fill="#5491C7"/><rect x="39.885" y="81.139" width="232" height="48" rx="24" transform="rotate(30 39.885 81.14)" fill="#4581B7"/></g></svg>
+      </div>
+    </div>
     <script>
       ${fs.readFileSync(
         require.resolve('@discoveryjs/json-ext/dist/json-ext.min.js'),
         'utf8'
       )}
     </script>
-    ${options.scripts
-      .map((item) => {
-        if (typeof item === 'string') {
-          item = { type: 'path', path: item };
-        }
-        return `<script>${
-          item.type === 'path'
-            ? fs.readFileSync(require.resolve(item.path), 'utf8')
-            : item.content
-        }</script>`;
-      })
-      .join('\n')}
+    ${options.scripts?.map((item) => handleAsset('script', item)).join('\n') ?? ''}
     <script>
       function _makeJsonExtAPI() {
         const jsonExtData = new Object(null);
@@ -101,24 +171,6 @@ function writeHeader(stream: Writable, options: Options): void {
       
       const ${options.jsonExtAPIName} = _makeJsonExtAPI();
     </script>
-    
-    <style>
-      html, body {
-        padding: 0;
-        margin: 0;
-        height: 100%;
-        border: none;
-        -webkit-text-size-adjust: 100%;
-      }
-      
-      #loading {
-        margin: 5px;
-        font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="loading">Loading...</div>
 `);
 }
 
